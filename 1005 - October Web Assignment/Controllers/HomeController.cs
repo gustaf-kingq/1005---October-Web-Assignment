54bf9f8a6bf32ad5ed707ecedc41c2214d7ae4d7
gustaf-kingq <gvmw@live.com>
2016-10-10 14:04:40 +0200

    1010 - Removed Recycled Code/Object Model Experience Notable changes: - Removed anything deviating from the tutorial. If you scramble it with the previous code it's just gets messy. - Added the Object model Experience to later get added to the Contact view.

--- 1005 - October Web Assignment/Controllers/HomeController.cs
+++ 1005 - October Web Assignment/Controllers/HomeController.cs
@@ -3,11 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using _1005___October_Web_Assignment.Models;
 
 namespace _1005___October_Web_Assignment.Controllers
 {
     public class HomeController : Controller
     {
+        private List<Experience> MyExperienceList;
+
+        public HomeController()
+        {
+            MyExperienceList = new List<Experience>();
+            MyExperienceList.Add(new Experience() { Title = "Målare", Description = "Målade farfars hus", Period = "2016" });
+            MyExperienceList.Add(new Experience() { Title = "Tryckare", Description = "Tryckte tidningar", Period = "2015-" });
+            MyExperienceList.Add(new Experience() { Title = "Försäljare", Description = "Sålde postkodlotter", Period = "2015" });
+        }
         // GET: Home
         public ActionResult Index()
         {
---
+++ 1005 - October Web Assignment/Models/Experience.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _1005___October_Web_Assignment.Models
+{
+    public class Experience
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Period { get; set; }
+    }
+}
