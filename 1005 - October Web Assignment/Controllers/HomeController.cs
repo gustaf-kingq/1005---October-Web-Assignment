39aa4fe09f559844018792e0e49f4aa401531e18
gustaf-kingq <gvmw@live.com>
2016-10-10 14:19:51 +0200

    1010 - Proper Experiences/A Fourth Example Notable changes: - Begun by adding proper examples to the object model, might even keep a few. - Added an extra example since I ran out of MyExperienceList adds.

--- 1005 - October Web Assignment/Controllers/HomeController.cs
+++ 1005 - October Web Assignment/Controllers/HomeController.cs
@@ -14,9 +14,30 @@ namespace _1005___October_Web_Assignment.Controllers
         public HomeController()
         {
             MyExperienceList = new List<Experience>();
-            MyExperienceList.Add(new Experience() { Title = "Målare", Description = "Målade farfars hus", Period = "2016" });
-            MyExperienceList.Add(new Experience() { Title = "Tryckare", Description = "Tryckte tidningar", Period = "2015-" });
-            MyExperienceList.Add(new Experience() { Title = "Försäljare", Description = "Sålde postkodlotter", Period = "2015" });
+            MyExperienceList.Add(new Experience()
+            {
+                Title = "Guldkretsen 2016 Finalist",
+                Description = "Together with Fredrik Svahn and Axel Ingelsson Fredler as co-creators, we created HTML5 game 'Steampunk and Napalm', a barebones top down space shooter.",
+                Period = "2015-16"
+            });
+            MyExperienceList.Add(new Experience()
+            {
+                Title = "RPG Maker:er",
+                Description = "Making RPG Maker games on a subregular basis with more creativity than raw code which makes the software more addicting and rewarding. My work can be seen on my other Github repo.",
+                Period = "2013-"
+            });
+            MyExperienceList.Add(new Experience()
+            {
+                Title = "Artist",
+                Description = "I enjoy drawing comics and cartoonish characters on my DeviantArt page under the name of QuakerII and has become a hobby since I feel more comfortable with it than programming.",
+                Period = "2010-"
+            });
+            MyExperienceList.Add(new Experience()
+            {
+                Title = "Gitarist",
+                Description = "I can play guitar pretty good. A lesser hobby but preferable over programming.",
+                Period = "2011-"
+            });
         }
         // GET: Home
         public ActionResult Index()
